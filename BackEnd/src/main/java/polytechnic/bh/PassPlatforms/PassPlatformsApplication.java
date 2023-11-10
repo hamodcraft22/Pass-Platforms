d439c992b2d7cae1548a6d04d56229d747a22bef
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-11-10 14:34:33 +0300

    added backend base Project

---
+++ BackEnd/src/main/java/polytechnic/bh/PassPlatforms/PassPlatformsApplication.java
@@ -0,0 +1,13 @@
+package polytechnic.bh.PassPlatforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassPlatformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassPlatformsApplication.class, args);
+	}
+
+}
