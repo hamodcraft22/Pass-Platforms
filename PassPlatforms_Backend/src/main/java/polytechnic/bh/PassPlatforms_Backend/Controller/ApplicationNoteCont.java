83f3ba6a259d277364641dd448ca804ddc74dfa8
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-11-20 01:54:13 +0300

    Update ApplicationNoteCont.java

--- PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Controller/ApplicationNoteCont.java
+++ PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Controller/ApplicationNoteCont.java
@@ -7,26 +7,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import polytechnic.bh.PassPlatforms_Backend.Dao.ApplicationNoteDao;
+import polytechnic.bh.PassPlatforms_Backend.Dto.GenericDto;
 import polytechnic.bh.PassPlatforms_Backend.Entity.ApplicationNote;
 import polytechnic.bh.PassPlatforms_Backend.Repository.ApplicationNoteRepo;
+import polytechnic.bh.PassPlatforms_Backend.Service.ApplicationNoteServ;
 
 import java.util.List;
 import java.util.Objects;
 
+import static polytechnic.bh.PassPlatforms_Backend.Constant.APIkeyConstant.ADMIN_KEY;
+import static polytechnic.bh.PassPlatforms_Backend.Constant.APIkeyConstant.MANAGER_KEY;
+
 @RestController
 @RequestMapping("/api/applicationNote")
-public class ApplicationNoteCont {
+public class ApplicationNoteCont<T> {
 
     @Autowired
-    ApplicationNoteRepo applicationNoteRepo;
+    ApplicationNoteServ applicationNoteServ;
 
     @GetMapping("")
-    public ResponseEntity<List<ApplicationNote>> getAllTutorials(@RequestHeader(value = "requestKey", required = false) String requestKey)
+    public ResponseEntity<GenericDto<T>> getAllNotes(
+            @RequestHeader(value = "requestKey", required = false) String requestKey)
     {
-        if (Objects.equals(requestKey, "student-3e1d-4e5f-a2b1-6c7d8e9f0a1b"))
+        if (Objects.equals(requestKey, ADMIN_KEY) || Objects.equals(requestKey, MANAGER_KEY))
         {
-            List<ApplicationNote> applicationNotes = applicationNoteRepo.findAll();
-            return new ResponseEntity<>(applicationNotes, HttpStatus.OK);
+            List<ApplicationNoteDao> retrivedNotes = applicationNoteServ.getAllAplicationNotes(false);
+
+            if (retrivedNotes != null && !retrivedNotes.isEmpty())
+            {
+                return new ResponseEntity<>(new GenericDto<>(null, (T) retrivedNotes, null), HttpStatus.UNAUTHORIZED);
+            }
+            else
+            {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
         }
         else
         {
