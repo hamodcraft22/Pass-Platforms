a013461dc6d7ba70cdfdf09eff6b6fa8f929849f
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-11-21 14:23:45 +0300

    added school service and controller

---
+++ PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Controller/SchoolCont.java
@@ -0,0 +1,132 @@
+package polytechnic.bh.PassPlatforms_Backend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import polytechnic.bh.PassPlatforms_Backend.Dao.SchoolDao;
+import polytechnic.bh.PassPlatforms_Backend.Dto.GenericDto;
+import polytechnic.bh.PassPlatforms_Backend.Service.SchoolServ;
+
+import java.util.List;
+import java.util.Objects;
+
+import static polytechnic.bh.PassPlatforms_Backend.Constant.APIkeyConstant.ADMIN_KEY;
+import static polytechnic.bh.PassPlatforms_Backend.Constant.APIkeyConstant.MANAGER_KEY;
+
+@RestController
+@RequestMapping("/api/school")
+public class SchoolCont
+{
+
+    @Autowired
+    private SchoolServ schoolServ;
+
+    // get all schools
+    @GetMapping("")
+    public ResponseEntity<GenericDto<List<SchoolDao>>> getAllSchools(
+            @RequestHeader(value = "Authorization", required = false) String requestKey)
+    {
+        // Any person can use these
+
+        List<SchoolDao> schools = schoolServ.getAllSchools();
+
+        if (schools != null && !schools.isEmpty())
+        {
+            return new ResponseEntity<>(new GenericDto<>(null, schools, null), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    // get school details
+    @GetMapping("/{schoolID}")
+    public ResponseEntity<GenericDto<SchoolDao>> getSchoolDetails(
+            @RequestHeader(value = "Authorization", required = false) String requestKey,
+            @PathVariable("schoolID") String schoolID)
+    {
+        // Any person can use these
+
+        SchoolDao school = schoolServ.getSchoolDetails(schoolID);
+
+        if (school != null)
+        {
+            return new ResponseEntity<>(new GenericDto<>(null, school, null), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    // create school
+    @PostMapping("")
+    public ResponseEntity<GenericDto<SchoolDao>> createSchool(
+            @RequestHeader(value = "Authorization", required = false) String requestKey,
+            @RequestBody SchoolDao schoolDao)
+    {
+        if (Objects.equals(requestKey, ADMIN_KEY) || Objects.equals(requestKey, MANAGER_KEY))
+        {
+            SchoolDao createdSchool = schoolServ.createSchool(schoolDao.getSchoolname(), schoolDao.getSchooldesc());
+
+            return new ResponseEntity<>(new GenericDto<>(null, createdSchool, null), HttpStatus.CREATED);
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+
+    }
+
+    // edit school
+    @PutMapping("")
+    public ResponseEntity<GenericDto<SchoolDao>> editSchool(
+            @RequestHeader(value = "Authorization", required = false) String requestKey,
+            @RequestBody SchoolDao schoolDao)
+    {
+
+        if (Objects.equals(requestKey, ADMIN_KEY) || Objects.equals(requestKey, MANAGER_KEY))
+        {
+            SchoolDao editedSchool = schoolServ.editSchool(schoolDao);
+
+            if (editedSchool != null)
+            {
+                return new ResponseEntity<>(new GenericDto<>(null, editedSchool, null), HttpStatus.OK);
+            }
+            else
+            {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    // delete school
+    @DeleteMapping("/{schoolID}")
+    public ResponseEntity<GenericDto<Void>> deleteSchool(
+            @RequestHeader(value = "Authorization", required = false) String requestKey,
+            @PathVariable("schoolID") String schoolID)
+    {
+        if (Objects.equals(requestKey, ADMIN_KEY) || Objects.equals(requestKey, MANAGER_KEY))
+        {
+            if (schoolServ.deleteSchool(schoolID))
+            {
+                return new ResponseEntity<>(null, HttpStatus.OK);
+            }
+            else
+            {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
+
--- PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Service/SchoolServ.java
+++ PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Service/SchoolServ.java
@@ -1,8 +1,73 @@
 package polytechnic.bh.PassPlatforms_Backend.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import polytechnic.bh.PassPlatforms_Backend.Dao.MajorDao;
+import polytechnic.bh.PassPlatforms_Backend.Dao.SchoolDao;
+import polytechnic.bh.PassPlatforms_Backend.Entity.School;
+import polytechnic.bh.PassPlatforms_Backend.Repository.SchoolRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SchoolServ
 {
+
+    @Autowired
+    private SchoolRepo schoolRepo;
+
+    public List<SchoolDao> getAllSchools()
+    {
+        List<SchoolDao> schools = new ArrayList<>();
+
+        for (School retrievedSchool : schoolRepo.findAll())
+        {
+            schools.add(new SchoolDao(retrievedSchool));
+        }
+
+        return schools;
+    }
+
+    public SchoolDao getSchoolDetails(String schoolID)
+    {
+        Optional<School> retrievedSchool = schoolRepo.findById(schoolID);
+
+        return retrievedSchool.map(SchoolDao::new).orElse(null);
+    }
+
+    public SchoolDao createSchool(String schoolName, String schoolDesc)
+    {
+        School newSchool = new School();
+
+        newSchool.setSchoolname(schoolName);
+        newSchool.setSchooldesc(schoolDesc);
+
+        return new SchoolDao(schoolRepo.save(newSchool));
+    }
+
+    public SchoolDao editSchool(SchoolDao updatedSchool)
+    {
+        Optional<School> retrievedSchool = schoolRepo.findById(updatedSchool.getSchoolid());
+
+        if (retrievedSchool.isPresent())
+        {
+            retrievedSchool.get().setSchoolname(updatedSchool.getSchoolname());
+            retrievedSchool.get().setSchooldesc(updatedSchool.getSchooldesc());
+
+            return new SchoolDao(schoolRepo.save(retrievedSchool.get()));
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public boolean deleteSchool(String schoolID)
+    {
+        schoolRepo.deleteById(schoolID);
+        return true;
+    }
 }
+
