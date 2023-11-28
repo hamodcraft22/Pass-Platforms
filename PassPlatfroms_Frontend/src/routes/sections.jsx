e4773faffb6cee7b8372bdb860624fa52014100c
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-11-28 21:08:57 +0300

    added base components and removed not needed items

---
+++ PassPlatfroms_Frontend/src/routes/sections.jsx
@@ -0,0 +1,38 @@
+import {lazy, Suspense} from 'react';
+import {Navigate, Outlet, useRoutes} from 'react-router-dom';
+
+import DashboardLayout from '../layouts/dashboard';
+
+export const IndexPage = lazy(() => import('../pages/app'));
+export const UserPage = lazy(() => import('../pages/user'));
+export const Page404 = lazy(() => import('../pages/page-not-found'));
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+    const routes = useRoutes([
+        {
+            element: (
+                <DashboardLayout>
+                    <Suspense>
+                        <Outlet/>
+                    </Suspense>
+                </DashboardLayout>
+            ),
+            children: [
+                {element: <IndexPage/>, index: true},
+                {path: 'user', element: <UserPage/>}
+            ],
+        },
+        {
+            path: '404',
+            element: <Page404/>,
+        },
+        {
+            path: '*',
+            element: <Navigate to="/404" replace/>,
+        },
+    ]);
+
+    return routes;
+}
