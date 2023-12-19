8fd06193503147751dd0ee78702b5ec638e0fea3
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-12-19 19:56:35 +0300

    added slot availability check

--- PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Repository/SlotRepo.java
+++ PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Repository/SlotRepo.java
@@ -16,6 +16,7 @@ public interface SlotRepo extends JpaRepository<Slot, Integer>
 
     Optional<Slot> findSlotByLeader_UseridAndSlotType_Typeid(String leaderID, char typeID);
 
+    // check if leader already has a slot within this time range
     @Transactional
     @Query(value = "select count(*) from pp_slot where leaderid = :leaderID and dayid = :dayID and ( (to_char(starttime + INTERVAL '5' MINUTE, 'HH24:MI:SS') < to_char(:startTime, 'HH24:MI:SS') and to_char(endtime - INTERVAL '5' MINUTE, 'HH24:MI:SS') > to_char(:startTime, 'HH24:MI:SS')) or (to_char(starttime + INTERVAL '5' MINUTE, 'HH24:MI:SS') < to_char(:endTime, 'HH24:MI:SS') and to_char(endtime - INTERVAL '5' MINUTE, 'HH24:MI:SS') > to_char(:endTime, 'HH24:MI:SS')) or (to_char(starttime, 'HH24:MI:SS') >= to_char(:startTime, 'HH24:MI:SS') and to_char(endtime, 'HH24:MI:SS') <= to_char(:endTime, 'HH24:MI:SS')) ) ", nativeQuery = true)
     int sameSlotTimeFind(String leaderID, char dayID, Timestamp startTime, Timestamp endTime);
--- PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Service/SlotServ.java
+++ PassPlatforms_Backend/src/main/java/polytechnic/bh/PassPlatforms_Backend/Service/SlotServ.java
@@ -9,16 +9,21 @@ import polytechnic.bh.PassPlatforms_Backend.Dao.UserDao;
 import polytechnic.bh.PassPlatforms_Backend.Dto.LeadersSlotsDto;
 import polytechnic.bh.PassPlatforms_Backend.Entity.Slot;
 import polytechnic.bh.PassPlatforms_Backend.Entity.User;
+import polytechnic.bh.PassPlatforms_Backend.Repository.BookingRepo;
 import polytechnic.bh.PassPlatforms_Backend.Repository.DayRepo;
 import polytechnic.bh.PassPlatforms_Backend.Repository.SlotRepo;
 import polytechnic.bh.PassPlatforms_Backend.Repository.SlotTypeRepo;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import static polytechnic.bh.PassPlatforms_Backend.Constant.BookingStatusConstant.BKNGSTAT_ACTIVE;
 import static polytechnic.bh.PassPlatforms_Backend.Util.UsersService.getAzureAdName;
 
 @Service
@@ -30,6 +35,9 @@ public class SlotServ
 
     @Autowired
     private DayRepo dayRepo;
+
+    @Autowired
+    private BookingRepo bookingRepo;
 
     @Autowired
     private UserServ userServ;
@@ -57,12 +65,13 @@ public class SlotServ
 
         for (String userID : userIDs)
         {
-            UserDao retrivedUser = userServ.getUser(userID);
-            if (retrivedUser.getRole().getRoleid() == 2)
+            UserDao retrievedUser = userServ.getUser(userID);
+
+            if (retrievedUser.getRole().getRoleid() == 2)
             {
                 List<SlotDao> slots = new ArrayList<>();
 
-                for (Slot retrievedSlot : slotRepo.findSlotsByLeader(new User(retrivedUser)))
+                for (Slot retrievedSlot : slotRepo.findSlotsByLeader(new User(retrievedUser)))
                 {
                     slots.add(new SlotDao(retrievedSlot.getSlotid(), retrievedSlot.getStarttime().toInstant(), retrievedSlot.getEndtime().toInstant(), retrievedSlot.getNote(), new SlotTypeDao(retrievedSlot.getSlotType()), new DayDao(retrievedSlot.getDay()), null));
                 }
@@ -75,6 +84,61 @@ public class SlotServ
         }
 
         return leaderSlots;
+    }
+
+    // get available slots by date range
+    public List<LeadersSlotsDto> getAvailableLeaderSlots(List<LeadersSlotsDto> leaderSlots, Date weekStartDate)
+    {
+        List<LeadersSlotsDto> correctedLeaderSlots = new ArrayList<>();
+
+        for (LeadersSlotsDto leadersSlotsDto : leaderSlots)
+        {
+
+            // slots to include
+            List<SlotDao> slotsToInclude = new ArrayList<>();
+
+            // inner lop for slots
+            for (SlotDao slotDao : leadersSlotsDto.getSlots())
+            {
+                LocalDate slotDateToWeek = null;
+
+                // get the date (respective to the start / end date)
+                switch (slotDao.getDay().getDayid())
+                {
+                    case 'U':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate();
+                    case 'M':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(1);
+                    case 'T':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(2);
+                    case 'W':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(3);
+                    case 'R':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(4);
+                    case 'F':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(5);
+                    case 'S':
+                        slotDateToWeek = weekStartDate.toInstant().atZone(ZoneId.of("Asia/Bahrain")).toLocalDate().plusDays(6);
+                }
+
+                // check db against date, if there is any active booking where date = gotten date and slot = gotten slot and status active
+                if (slotDateToWeek != null)
+                {
+                    if (!bookingRepo.existsBySlot_SlotidAndBookingdateAndBookingStatus_Statusid(slotDao.getSlotid(),Date.from(slotDateToWeek.atStartOfDay(ZoneId.of("Asia/Bahrain")).toInstant()),BKNGSTAT_ACTIVE))
+                    {
+                        slotsToInclude.add(slotDao);
+                    }
+                }
+
+            }
+
+            if (!slotsToInclude.isEmpty())
+            {
+                LeadersSlotsDto correctedleadersSlotsDto = new LeadersSlotsDto(leadersSlotsDto.getLeaderID(), leadersSlotsDto.getLeaderName(), slotsToInclude);
+            }
+        }
+
+        return correctedLeaderSlots;
     }
 
     // get a single slot and its details - also not needed
